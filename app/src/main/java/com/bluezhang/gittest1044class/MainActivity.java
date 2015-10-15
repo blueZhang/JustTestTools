6eee0520ee1acd7ba54f868f3b0b7b60131db5c4
blueZhang <2029379603@qq.com>
2015-10-15 14:44:33 +0800

    second commit 添加按钮以及异步加载图片

--- app/src/main/java/com/bluezhang/gittest1044class/MainActivity.java
+++ app/src/main/java/com/bluezhang/gittest1044class/MainActivity.java
@@ -4,14 +4,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import com.bluezhang.bitmapdisplay_1012_class.imagesutil.MD5ImageDownload;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button ;
+    private ImageView imageView;
+    private TextView textView ;
+
+    private Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        imageView = (ImageView) findViewById(R.id.iamgeView);
+        textView = (TextView) findViewById(R.id.textView);
+        animation = AnimationUtils.loadAnimation(this,R.anim.image_animation);
+        imageView.setAnimation(animation);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.baidu.com/img/bdlogo.png";
+                MD5ImageDownload.loadImageFromUrl(getApplicationContext(), url, imageView, 200, 200);
+
+
+            }
+        });
+
+
+
+
     }
 
     @Override
